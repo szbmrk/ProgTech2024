fecc2a8e2d678e085e92c957b2bc4c9f8b3c5535
szbmrk <szmark16@gmail.com>
2024-04-28 13:08:40 +0200

    listing transactions

--- app/src/main/java/com/progtech/progtech2024/database/TransactionDao.java
+++ app/src/main/java/com/progtech/progtech2024/database/TransactionDao.java
@@ -2,9 +2,15 @@ package com.progtech.progtech2024.database;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface TransactionDao {
     @Insert
     long insert(Transaction transaction);
+
+    @Query("SELECT * FROM TRANSACTIONS WHERE fromAccountId = :userId OR toAccountId = :userId")
+    List<Transaction> getTransactionsForUser(int userId);
 }
