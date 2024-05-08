c52f2c938fb88ad995caaf944cdebd58fb39e593
szbmrk <szmark16@gmail.com>
2024-05-08 10:26:03 +0200

    formatting

--- app/src/androidTest/java/com/progtech/progtech2024/commands/WithdrawCommandUnitTest.java
+++ app/src/androidTest/java/com/progtech/progtech2024/commands/WithdrawCommandUnitTest.java
@@ -43,6 +43,7 @@ public class WithdrawCommandUnitTest {
                 InstrumentationRegistry.getInstrumentation().getTargetContext(),
                 600, account
         );
+
         try {
             withdrawCommand.TestCall();
         }
